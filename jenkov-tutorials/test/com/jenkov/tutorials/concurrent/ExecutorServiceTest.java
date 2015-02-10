ca62e968843303648734d7a539fb4b2d7bd93571
suhyuk-park <park.suhyuk@gmail.com>
2015-02-10 23:41:58 +0900

    initial import

---
+++ jenkov-tutorials/test/com/jenkov/tutorials/concurrent/ExecutorServiceTest.java
@@ -0,0 +1,21 @@
+package com.jenkov.tutorials.concurrent;
+
+import junit.framework.TestCase;
+
+public class ExecutorServiceTest extends TestCase {
+
+	protected static void setUpBeforeClass() throws Exception {
+	}
+
+	protected static void tearDownAfterClass() throws Exception {
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
